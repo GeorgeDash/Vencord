0e9c9450a3cbc0976ad2285b96493eaad63f66e2
GeorgeDash <georgi.milen.zhelev@gmail.com>
2025-06-18 18:44:44 +0300

    .

--- src/main/csp/index.ts
+++ src/main/csp/index.ts
@@ -22,6 +22,8 @@ export const CspPolicies: PolicyMap = {
     "localhost": ImageAndCssSrc,
     "127.0.0.1": ImageAndCssSrc,
 
+
+    "fakeprofile.is-always.online": ImageAndCssSrc, // FakeProfile API
     "*.github.io": ImageAndCssSrc, // GitHub pages, used by most themes
     "github.com": ImageAndCssSrc, // GitHub content (stuff uploaded to markdown forms), used by most themes
     "raw.githubusercontent.com": ImageAndCssSrc, // GitHub raw, used by some themes
